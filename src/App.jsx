7c9352b9d42f2ee4a56e52341737cae33699278f
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-27 15:09:13 +0300

    new project new me xD

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div>HELLO!</div>;
+  return <Home></Home>;
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="bg-indigo-500 text-center text-3xl text-gray-200">
+      HELLO WORLD!
+    </div>
+  );
+}
+
+export default Home;
