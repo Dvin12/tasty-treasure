c28bd9a37bff7283ed47590792228f411d8b6976
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-06 15:55:45 +0300

    styled the components

--- src/components/VegetarianCard.jsx
+++ src/components/VegetarianCard.jsx
@@ -1,5 +1,22 @@
-function VegetarianCard() {
-  return <div>VEGETARIAN CARD!</div>;
+function VegetarianCard({ vegetarianData }) {
+  console.log(vegetarianData);
+  return (
+    <section className="my-4  ">
+      <div className="w-[300px] relative overflow-hidden  flex items-center justify-center cursor-pointer">
+        <img
+          src={vegetarianData.image}
+          alt={vegetarianData.title}
+          className="rounded-3xl object-cover w-full h-full "
+        />
+
+        <div className="bg-gray-900 absolute w-full h-full rounded-3xl opacity-0 hover:opacity-90 duration-300 flex items-center justify-center ">
+          <h4 className="  absolute text-white text-md text-center font-medium   w-52 ">
+            {vegetarianData.title}
+          </h4>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default VegetarianCard;
--- src/components/VegetarianPicks.jsx
+++ src/components/VegetarianPicks.jsx
@@ -1,14 +1,29 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
 import VegetarianCard from "./VegetarianCard";
 
 function VegetarianPicks({ vegetarian }) {
-  console.log(vegetarian);
   return (
-    <section className="mx-60 p-8 my-10">
-      <h2 className="text-3xl font-medium">Most Viewed Vegetarian Recipes</h2>
+    <section className="mx-60  my-20">
+      <h2 className="text-3xl font-medium px-8">
+        Most Viewed Vegetarian Recipes
+      </h2>
+
       <div>
-        {vegetarian.slice(0, 4).map((item) => (
-          <VegetarianCard></VegetarianCard>
-        ))}
+        <Splide
+          options={{
+            perPage: 4,
+            padding: 20,
+            gap: 20,
+            pagination: false,
+          }}
+        >
+          {vegetarian.map((item) => (
+            <SplideSlide key={item.id}>
+              <VegetarianCard vegetarianData={item}></VegetarianCard>
+            </SplideSlide>
+          ))}
+        </Splide>
       </div>
     </section>
   );
