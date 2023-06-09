4ac37e926e520976058993892989c8e21906ae10
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-09 19:59:51 +0300

    resolve an issue with maping through array

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Recipe() {
-  const [recipeDetails, setRecipeDetails] = useState({});
+  const [recipeDetails, setRecipeDetails] = useState([{}]);
   let params = useParams();
   const getRecipeDetails = async () => {
     const data = await fetch(
@@ -18,7 +18,28 @@ function Recipe() {
     getRecipeDetails();
   }, [params.name]);
 
-  return <div>You are viewing: {recipeDetails.title}</div>;
-}
+  console.log(recipeDetails);
 
+  return (
+    <section className="bg-gray-200">
+      <div className="grid grid-cols-2 mx-48 p-12">
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="text-xl py-4">{recipeDetails.title}</h3>
+          <img src={recipeDetails.image} alt="" />
+        </div>
+        <div>
+          <ul className="grid grid-cols-2"></ul>
+          {recipeDetails.extendedIngredients ? (
+            recipeDetails.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.nameClean}</li>
+            ))
+          ) : (
+            <li>Loading ingredients...</li>
+          )}
+          <h4>{recipeDetails.instructions}</h4>
+        </div>
+      </div>
+    </section>
+  );
+}
 export default Recipe;
