fc7935984262af188d08e1e5d01a3d4995a95915
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-09 18:28:58 +0300

    added fetch to get recipe info by id

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Recipe() {
-  return <div>Recipe Page!</div>;
+  const [recipeDetails, setRecipeDetails] = useState({});
+  let params = useParams();
+  const getRecipeDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    const details = await data.json();
+    setRecipeDetails(details);
+  };
+
+  useEffect(() => {
+    getRecipeDetails();
+  }, [params.name]);
+
+  return <div>You are viewing: {recipeDetails.title}</div>;
 }
 
 export default Recipe;
