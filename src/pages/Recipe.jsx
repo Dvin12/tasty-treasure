6ef610818f14ec9e546db2216334fae227d56e21
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-10 17:16:53 +0300

    added styling to the Recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
+import Search from "../components/Search";
 
 function Recipe() {
   const [recipeDetails, setRecipeDetails] = useState([{}]);
@@ -22,26 +23,48 @@ function Recipe() {
   console.log(recipeDetails);
 
   return (
-    <section className="bg-gray-200">
+    <section className="bg-gray-200 ">
       <motion.div
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 2 }}
       >
-        <div className="grid grid-cols-2 mx-48 p-12">
+        <Search></Search>
+        <div className="grid grid-cols-2 mx-48 p-12 justify-items-center items-center h-fit">
           <div className="flex flex-col items-center justify-center">
-            <h3 className="text-xl py-4">{recipeDetails.title}</h3>
-            <img src={recipeDetails.image} alt="" />
+            <img
+              src={recipeDetails.image}
+              alt={recipeDetails.title}
+              className="rounded-2xl"
+            />
+            <button className="p-4 bg-green-500 my-8 rounded-xl font-medium text-gray-100 text-lg">
+              Add to Favourites
+            </button>
+            <Link to="/recipes">
+              <button>Go Back</button>
+            </Link>
           </div>
-          <div>
-            <ul className="grid grid-cols-2"></ul>
-            {recipeDetails.extendedIngredients
-              ? recipeDetails.extendedIngredients.map((ingredient) => (
-                  <li key={ingredient.id}>{ingredient.nameClean}</li>
-                ))
-              : ""}
+          <div className="bg-gray-100 p-8 rounded-2xl h-fit">
+            <h3 className="text-2xl py-4">{recipeDetails.title}</h3>
+            <span
+              className="text-md"
+              dangerouslySetInnerHTML={{ __html: recipeDetails.summary }}
+            ></span>
+            <div className="p-8 border-t-2 border-b-2 my-10">
+              <ul className="grid grid-cols-3">
+                {recipeDetails.extendedIngredients
+                  ? recipeDetails.extendedIngredients.map((ingredient) => (
+                      <li className="my-2 list-disc" key={ingredient.id}>
+                        {ingredient.nameClean}
+                      </li>
+                    ))
+                  : ""}
+              </ul>
+            </div>
+            <h3 className="text-xl text-center">Instructions</h3>
             <h4
+              className="p-4 my-4"
               dangerouslySetInnerHTML={{ __html: recipeDetails.instructions }}
             ></h4>
           </div>
