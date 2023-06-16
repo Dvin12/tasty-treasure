f509a700dab97a5001819e8605d772e6f21060b7
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-16 16:04:49 +0300

    implemented all the necessary functionality

--- src/components/SavedItem.jsx
+++ src/components/SavedItem.jsx
@@ -1,14 +1,25 @@
 import { FaTrash } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { removeFromSaved } from "../redux/tastySlice";
+import { Link } from "react-router-dom";
 
 function SavedItem({ item }) {
+  const dispatch = useDispatch();
   return (
     <div className="">
-      <img src={item.image} className="rounded-2xl cursor-pointer "></img>
-      <div className="flex items-center justify-between">
-        <h3 className="text-xl my-4 font-medium cursor-pointer">
-          {item.title}
-        </h3>
-        <FaTrash className="text-xl cursor-pointer"></FaTrash>
+      <Link to={"/recipe/" + item.id}>
+        <img src={item.image} className="rounded-2xl cursor-pointer "></img>
+      </Link>
+      <div className="flex items-center p-1 justify-between">
+        <Link to={"/recipe/" + item.id}>
+          <h3 className="text-xl my-4 font-medium cursor-pointer w-56">
+            {item.title}
+          </h3>
+        </Link>
+        <FaTrash
+          onClick={() => dispatch(removeFromSaved(item.id))}
+          className="text-xl cursor-pointer"
+        ></FaTrash>
       </div>
     </div>
   );
--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -1,21 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SavedItem from "../components/SavedItem";
-import Search from "../components/Search";
+
+import { resetSaved } from "../redux/tastySlice";
 
 function Saved() {
   const savedData = useSelector((state) => state.tasty.recipeDetails);
+  const dispatch = useDispatch();
   return (
     <section className="h-fit bg-gray-200">
       <div className="py-60 mx-60">
         <div className=" flex items-center justify-between">
           <h3 className=" font-medium text-4xl">Saved Recipes</h3>
-          <span className="font-semibold text-xl text-green-800 cursor-pointer">
+          <span
+            onClick={() => dispatch(resetSaved())}
+            className="font-semibold text-xl text-green-800 cursor-pointer"
+          >
             Remove All
           </span>
         </div>
         <div className="grid grid-cols-3 my-40 items-center justify-items-center gap-24">
           {savedData.map((item) => (
-            <SavedItem item={item}></SavedItem>
+            <SavedItem item={item} key={item.id}></SavedItem>
           ))}
         </div>
       </div>
