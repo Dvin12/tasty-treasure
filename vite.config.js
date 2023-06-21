194183f8af0ed6e1aff74c31915406e7d3046b3b
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-21 19:08:05 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tasty-treasure/",
+  base: "/tasty-treasure",
   server: {
     port: 3000,
   },
